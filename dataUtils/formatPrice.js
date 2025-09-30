daa0ab7dc238882f00b8aac81e2e58bc5e00333b
thiswhy <1414770848@qq.com>
2025-09-30 10:30:53 +0800

    修改精度问题

--- dataUtils/formatPrice.js
+++ dataUtils/formatPrice.js
@@ -1,6 +1,9 @@
-const Decimal =require('decimal.js')
+const Decimal = require('decimal.js')
 
 export const formatPrice = (price, step) => {
     const p = new Decimal(Number(price)).div(Number(step)).floor().times(Number(step)).toNumber()
+    if (p < 0) {
+        return `0`
+    }
     return `${p}`
 }
